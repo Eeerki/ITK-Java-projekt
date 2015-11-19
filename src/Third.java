a2bffe175d761466c4d3deda90c40a792b178b69
unknown <Erki@LAP02.lmkeskus.ee>
2015-11-19 19:36:42 +0200

    Täiendasin projekti

--- src/Third.java
+++ src/Third.java
@@ -1,17 +1,15 @@
-import com.sun.corba.se.pept.transport.ContactInfoList;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import javafx.application.Application;
-import javafx.event.*;
 import javafx.geometry.Insets;
-import javafx.scene.*;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
-import javafx.stage.*;
-
-import java.nio.file.StandardWatchEventKinds;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Third extends Application {
 
@@ -31,13 +29,11 @@ public class Third extends Application {
         primaryStage.setResizable(false);
 
 
-        // Põhjas asuv ActionBar
-
-
         // Põhiline Layout BorderPane
         BorderPane Raamistik = new BorderPane();
         Raamistik.setBottom(BottomMenu());
 
+        Raamistik.setPadding(new Insets(10,10,10,10));
 
 
         Scene scene = new Scene(Raamistik, 600, 600);
@@ -49,20 +45,31 @@ public class Third extends Application {
 
     // Meetodid siit alla
 
-    private GridPane CenterGrid(){
 
-        return null;
+
+    private void KeskneTekst(TextField userInput) {
+
+        GridPane Keskne = new GridPane();
+        Label centerText = new Label(userInput.toString());
+        GridPane.setConstraints(centerText,0,0);
+        Keskne.getChildren().addAll(centerText);
+
+
+
+
     }
 
 
+    private HBox BottomMenu() {
 
-    private HBox BottomMenu (){
         // Teen Hbox stiilis
         HBox hList = new HBox(10);
+        hList.setSpacing(8);
 
         // Asjad, mida ma lisan Hboxi
         // CheckBox
         CheckBox checkbox1 = new CheckBox("Suur");
+
 
         // TextField
         TextField userInput = new TextField();
@@ -70,21 +77,20 @@ public class Third extends Application {
 
         // LocalButton
         Button localButton = new Button("Kliki siia");
-        localButton.setOnAction(e-> localButtonAction(userInput, checkbox1));
+        localButton.setOnAction(e -> localButtonAction(userInput, checkbox1));
 
         // Lisan kõik listi
-        hList.getChildren().addAll(userInput,checkbox1,localButton);
+        hList.getChildren().addAll(userInput, checkbox1, localButton);
 
         return hList;
     }
-    private String localButtonAction(TextField userInput, CheckBox checkBox1){
 
-        if(checkBox1.isSelected()){
+    private String localButtonAction(TextField userInput, CheckBox checkBox1) {
 
-            System.out.println("LocalButtonAction tagastas Stringi");
+        if (checkBox1.isSelected()) {
+            KeskneTekst(userInput);
             return userInput.getText();
-        }
-        else{
+        } else {
             System.out.println("None");
         }
         return null;
@@ -95,17 +101,26 @@ public class Third extends Application {
 
 
 
-        private boolean isInt (TextField input, String message){
-            try{
-                int age = Integer.parseInt(input.getText());
-                        System.out.println("User is " + age);
-                return true;
-            }
-            catch(NumberFormatException e){
-                System.out.println("Error ˇ" + message + "ˇ is not a valid number");
-                return false;
-            }
+
+
+
+
+
+
+
+
+
+    private boolean isInt(TextField input, String message) {
+        try {
+            int age = Integer.parseInt(input.getText());
+            System.out.println("User is " + age);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Error ˇ" + message + "ˇ is not a valid number");
+            return false;
         }
+    }
+
     private void handleOptions(CheckBox box1, CheckBox box2) {
 
         String message = "";
@@ -120,7 +135,6 @@ public class Third extends Application {
             System.out.println(message);
         }
     }
-
 
 
 }
